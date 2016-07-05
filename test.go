53dd80ca3033abc3dde2481b17c6f3f16ef5b19d
haodriver <hao@drivergrp.com>
2016-07-05 10:23:34 -0700

    Add files via upload
    added Test function for WriteMessage

--- test.go
+++ test.go
@@ -1,19 +1,40 @@
 package main
 
 import (
-	"fmt"
-
-	"time"
+	//"github.com/stretchr/testify/mock"
+	"os"
+	"serial"
+	"testing"
 )
 
-func main() {
-	weight := "0.02"
-	dataSlice := ""
-	dataSlice += time.Now().String()
-	dataSlice += "\n"
-	dataSlice += weight
-	fmt.Println(dataSlice)
-	/*err := ioutil.WriteFile(logFile, time.Now() weight, perm)
-	check(err)
+/* Test WriteMessage
+ */
+
+func TestWriteMessage(t *testing.T) string {
+	/*
+		basic_test.go catches emtpy/inaccessble port
 	*/
+
+	port0 := os.Getenv("PORT0")
+	c0 := &serial.Config{Name: port0, Baud: 9600}
+	s0, err := serial.OpenPort(c0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// test case of writing an empty message
+	if WriteMessage(s0, "") != "Empty Command" {
+		return "missing empty command case"
+	}
+	// test case of writing a message without proper <cr>
+	if WriteMessage(s0, "Q") != "No <cr> and <lf> at the end of the command" {
+		return "missing incomplete command case "
+	}
+	return "YAYYYY"
+	// test case of writing a proper message
+
 }
+
+//func TestReadMessage(t *testing.T) {
+
+//}
